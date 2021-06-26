d1bcf995b273aba2e3cffc77654eabbf820f630a
Cristian <cristianmauriciogutierrez1998@gmail.com>
2021-06-26 18:49:22 -0500

    Front Realizado sin Back

---
+++ src/reducers/authReducer.js
@@ -0,0 +1,27 @@
+import { types } from "../types/types";
+
+const initialState = {
+    checking: true
+}
+
+export const authReducer = (state= initialState, action) => {
+
+    switch (action.type) {
+        
+        case types.authLogin:
+            return {
+                ...state,
+                checking: false,
+                ...action.payload
+            }
+        case types.authStartRegister:
+            return {
+                
+            }
+
+    
+        default:
+            return state;
+    }
+    
+}
---
+++ src/types/types.js
@@ -0,0 +1,20 @@
+
+export const types = {
+    uiOpenModal: '[ui] Open Modal',
+    uiCloseModal: '[ui] Close Modal',
+
+    eventSetActive: '[event] Set Active',
+    eventAddNew: '[event] Add New',
+    eventClearActiveEvent: '[event] Clear Event',
+    eventUpdated: '[event] Event Updated',
+    eventDeleted: '[event] Event Deleted',
+
+    authChecking: '[auth] Checking login state',
+    authCheckingFinish: '[auth] Finish checking login state',
+    authStartLogin: '[auth] Start login',
+    authLogin: '[auth] login',
+    authStartRegister: '[auth] Start register',
+    authStartTokenRenew: '[auth] Start token renew',
+    authLogout: '[auth] Logout',
+
+}
